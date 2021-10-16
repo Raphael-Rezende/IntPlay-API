a33838f4db824d971fbf22adfa67ea29f1e7b250
Raphael Rezende <raphaemanuel@gmail.com>
2021-10-16 17:37:14 -0300

    pc failed

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -44,67 +44,6 @@ class MovieController {
       console.log(err)
     }
   }
-  async find(req, res) {
-    const { id } = req.params;
-    const errors = translateJson.duvidas.find;
-
-    try {
-
-      const duvida = await Duvida.findById(id);
-
-      return res.json(duvida);
-    }
-    catch (err) {
-      throw createError(err.status, { errors })
-    }
-  }
-
-  async update(req, res) {
-    const { id } = req.params;
-    const { lang } = req.params;
-    const errors = translateJson.duvidas.update;
-    try {
-      const duvida = await Duvida.findByIdAndUpdate(id, { ...req.body }, { new: true });
-      const dataUpdate = Date.now();
-      if (duvida) {
-        AtualizacaoController.includeInternal('duvidas', duvida.lingua, dataUpdate)
-      }
-
-      return res.json(duvida);
-    }
-    catch (err) {
-      throw createError(err.status, { errors })
-    }
-  }
-
-  async deleted(req, res) {
-    const errors = translateJson.duvidas.delete;
-    const { id } = req.params;
-
-    try {
-      const duvida = await Duvida.findByIdAndUpdate(id, { deleted: true });
-
-      return res.json(duvida);
-    }
-    catch (err) {
-      console.log(err)
-      throw createError(err.status, { errors })
-    }
-  }
-
-  async delete(req, res) {
-    const errors = translateJson.duvidas.delete;
-    const { id } = req.params;
-
-    try {
-      const duvida = await Duvida.findByIdAndRemove(id);
-      return res.json(duvida);
-    }
-    catch (err) {
-      throw createError(err.status, { errors })
-    }
-  }
-
 }
 
 module.exports = new MovieController();
